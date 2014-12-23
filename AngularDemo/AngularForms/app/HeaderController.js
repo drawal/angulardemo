3c640c8c66df2a12ada32d6c7b551ef027d97984
Deependra Rawal <deependra009@yahoo.com>
2014-12-23 02:35:13 -0600

    Added Contract and WebApi

---
+++ AngularDemo/AngularForms/app/HeaderController.js
@@ -0,0 +1,7 @@
+﻿angularFormsApp.controller('HeaderController',
+
+function HeaderController($scope, $location) {
+    $scope.isActive = function (viewLocation) {
+        return viewLocation === $location.path();
+    };
+});
---
+++ AngularDemo/AngularForms/app/basicDirective.js
@@ -0,0 +1,9 @@
+﻿angularFormsApp.directive('div', function () {
+    var directive = {};
+
+    directive.restrict = 'E'; /* restrict this directive to elements */
+
+    directive.template = "My first directive: {{textToInsert}}";
+
+    return directive;
+});
